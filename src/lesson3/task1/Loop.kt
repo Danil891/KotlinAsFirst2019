4f616cf955f0136ecdeaa9d3d33f16b999517020
Danil <cherepkov.dd@edu.spbstu.ru>
2019-10-05 22:33:44 +0300

    Часть 3 урока, еще думаю как оптимизировать задачу из 2

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -124,6 +124,7 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
+
 fun maxDivisor(n: Int): Int {
     var a: Int = n - 1
     while (n % a != 0) a--
